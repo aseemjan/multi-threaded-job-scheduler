db8325f29f73174aac10745c215c3d9a3969413a
Aseem <aseemjan7@gmail.com>
2025-12-27 13:48:16 +0530

    feat: wire execution strategies with  executor service

--- src/main/java/com/scheduler/jobscheduler/execution/executor/ScheduledJobExecutor.java
+++ src/main/java/com/scheduler/jobscheduler/execution/executor/ScheduledJobExecutor.java
@@ -1,9 +1,20 @@
 package com.scheduler.jobscheduler.execution.executor;
 
+import com.scheduler.jobscheduler.execution.pool.ThreadPoolProvider;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class ScheduledJobExecutor implements JobExecutor{
+
+    private final ExecutorService executorService;
+
+    public ScheduledJobExecutor(){
+        this.executorService = ThreadPoolProvider.scheduledPool();
+    }
 
     @Override
     public void execute(Runnable task){
-        //TODO : implement in next phase
+        executorService.submit(task);
     }
 }
--- src/main/java/com/scheduler/jobscheduler/execution/executor/SimpleJobExecutor.java
+++ src/main/java/com/scheduler/jobscheduler/execution/executor/SimpleJobExecutor.java
@@ -1,9 +1,19 @@
 package com.scheduler.jobscheduler.execution.executor;
 
+import com.scheduler.jobscheduler.execution.pool.ThreadPoolProvider;
+
+import java.util.concurrent.ExecutorService;
+
 public class SimpleJobExecutor implements JobExecutor{
+
+    private final ExecutorService executorservice;
+
+    public SimpleJobExecutor(){
+        this.executorservice = ThreadPoolProvider.immediatePool();
+    }
 
     @Override
     public void execute(Runnable task) {
-        //TODO : implement in next phase
+        executorservice.submit(task);
     }
 }
